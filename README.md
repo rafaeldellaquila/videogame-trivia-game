8e2f7126f164e1476a5c8a64d989567f0c44d35a
Rafael Dell'Aquila <rafael@dellaquila.dev>
2021-12-16 13:35:16 -0300

    :tada: new project start

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # videogame-trivia-game
 
+🎮 Trivia game based on Vue.js and Open Trivia DB.
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>
+      Microphones can be used not only to pick up soung, but also to project
+      sound similar to a speaker.
+    </h1>
+
+    <input type="radio" name="options" value="true" />
+    <label for="option">True</label> <br />
+
+    <input type="radio" name="options" value="false" />
+    <label for="option">False</label><br />
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {}
+  },
+  created() {
+    this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=15')
+      .then((response) => {
+        console.log(response.data)
+      })
   }
 }
 </script>
@@ -21,6 +40,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type='radio'] {
+    margin: 12px 4px;
+  }
+
+  .send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: white;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
